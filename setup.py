fa29787feb137e176ab20625bb86c377d86202af
Roman Kozlovskiy <krzroman@gmail.com>
2012-10-11 00:21:22 +0300

    Fixed classifiers

--- setup.py
+++ setup.py
@@ -21,10 +21,8 @@ setup(name='collective.embedly',
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
-        "Framework :: Plone :: 4.0",
         "Framework :: Plone :: 4.1",
         "Framework :: Plone :: 4.2",
-        "Framework :: Plone :: 4.3",
         "Programming Language :: Python",
         "Development Status :: 5 - Production/Stable",
         "Environment :: Plugins",
